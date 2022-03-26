e6923391dca4d020dbd71634af02170f650c4ef2
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-03-26 18:09:19 -0400

    feat(recursion): `binarySearch`

--- src/topics/01-complexity-analysis-and-recursion/02-recursion/exercises/search.ts
+++ src/topics/01-complexity-analysis-and-recursion/02-recursion/exercises/search.ts
@@ -4,26 +4,26 @@
  */
 
 /**
- * Given an array of integers, `nums`, the function returns the index of `num`.
- *
- * If `num` is not found, `-1` will be returned.
+ * Given an integer array, `nums`, the function returns the index of `target`,
+ * if found. If not found, the function will return `-1`.
  *
  * @example search([1, 2, 3, 4, 5], 5) => 4
  * @example search([1, 2, 3, 4, 5], 15) => -1
  *
  * @param {number[]} nums - Integer array
- * @param {number} num - Number to search for in `nums`
- * @return {number} Index of `num` if found, `-1` otherwise
+ * @param {number} target - Number to search for in `nums`
+ * @return {number} Index of `target` if found, `-1` otherwise
  */
-const search = (nums: number[], num: number): number => {
+const search = (nums: number[], target: number): number => {
   // Base case 1: If nums is empty array, return -1
   if (nums.length === 0) return -1
 
   /** @const {number} index - Current index */
   const index: number = nums.length - 1
 
-  // Base case 2: If num was found, return index. Otherwise, continue search
-  return nums[index] === num ? index : search(nums.slice(0, index), num)
+  // Base case 2: If target was found, return index
+  // Otherwise, continue searching for index of target
+  return nums[index] === target ? index : search(nums.slice(0, index), target)
 }
 
 export default search
