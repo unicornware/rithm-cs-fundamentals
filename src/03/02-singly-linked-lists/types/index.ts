a9c0de65f7fdb6f43862025c352b99f3f2e2765f
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-03-31 18:26:39 -0400

    feat(linked-lists): `Val`

---
+++ src/03/02-singly-linked-lists/types/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @file Entry Point - Singly Linked Lists Type Definitions
+ * @module csf/topics/singly-linked-lists/types
+ */
+
+export type { default as Val } from './val.type'
---
+++ src/03/02-singly-linked-lists/types/val.type.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Singly Linked Lists Type Definitions - Val
+ * @module csf/topics/singly-linked-lists/types/Val
+ */
+
+import type { JSONPrimitive } from '@flex-development/tutils'
+
+/**
+ * Linked list node value type.
+ */
+type Val = JSONPrimitive
+
+export default Val
